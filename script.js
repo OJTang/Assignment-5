dac7fc344afb2020bd1d6b42db6a45448d384fe2
OJTang <oliverwunderlich13@gmail.com>
2021-04-11 03:36:07 -0500

    playing with autograder

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ window.addEventListener("load", function() {
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
-   listedPlanetsResponse.then(function (json){
-
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       let missionTarget = document.getElementById("missionTarget");
+   listedPlanetsResponse.then(function (json) {
+            let missionTarget = document.getElementById("missionTarget");
 
             let i = pickPlanet(json);
 
